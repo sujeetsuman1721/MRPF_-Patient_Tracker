30922f31b3f33c0b5f1d0bf047fefb1d5e980bd7
Harini <2088499@cognizant.com>
2022-03-25 09:47:32 +0530

    Mvc added

---
+++ PatientTracker/PatientTracker/Models/DTOs/DoctorRegistrationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientTracker.Models.DTOs
+{
+    public class DoctorRegistrationModel:RegistrationModel
+    {
+        [Required]
+        public string Qualification { get; set; }
+        [Required]
+        public string Specialization { get; set; }
+    }
+}
---
+++ PatientTracker/PatientTracker/Models/Services/PatientRegistrationServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientTracker.Models.Services
+{
+    public class PatientRegistrationServices
+    {
+        private readonly HttpClient client;
+        public PatientRegistrationServices(IHttpClientFactory factory)
+        {
+            client = factory.CreateClient("PatientRegistrationServicesAPI");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+        public async Task<bool> SaveRegister(PatientRegistrationModel register)
+        {
+            var Json = JsonConvert.SerializeObject(register);
+            var Content = new StringContent(Json, Encoding.UTF8, "application/json");
+
+            var Response = await client.PostAsync("/api/patientsauth", Content);
+            Response.EnsureSuccessStatusCode();
+            return Response.StatusCode == HttpStatusCode.Created;
+        }
+    }
+}
