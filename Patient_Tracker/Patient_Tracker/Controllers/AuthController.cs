697a6fc6d4804e964b6bf163e85796fc6b403fe9
harinipallanti00 <harinipallanti00@gmail.com>
2022-03-27 16:09:24 +0530

    add

---
+++ Patient_Tracker/Patient_Tracker/Controllers/AuthController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Patient_Tracker.Models.DTOs;
+using Patient_Tracker.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patient_Tracker.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserServices context;
+
+        public AuthController(UserServices context) : base()
+        {
+            this.context = context;
+        }
+        public IActionResult PatientRegister()
+        {
+            return View();
+        }
+        [HttpPost("Patient")]
+        public async Task<IActionResult> PatientRegister(Patient model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var IsAdded = await context.SavePatient(model);
+            if (IsAdded)
+                return RedirectToAction("Index");
+
+            ModelState.AddModelError("", "Failed to do registration");
+            return View(model);
+
+
+        }
+        public IActionResult DoctorRegister()
+        {
+            return View();
+        }
+        [HttpPost("Doctor")]
+        public async Task<IActionResult> DoctorRegister(Doctor model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var IsAdded = await context.SaveDoctor(model);
+            if (IsAdded)
+                return RedirectToAction("Index");
+
+            ModelState.AddModelError("", "Failed to do registration");
+            return View(model);
+
+
+        }
+        public IActionResult ClerkRegister()
+        {
+            return View();
+        }
+        [HttpPost("Clerk")]
+        public async Task<IActionResult> ClerkRegister(Clerk model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var IsAdded = await context.SaveClerk(model);
+            if (IsAdded)
+                return RedirectToAction("Index");
+
+            ModelState.AddModelError("", "Failed to do registration");
+            return View(model);
+
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Patient_Tracker/Patient_Tracker/Models/Services/UserServices.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Patient_Tracker.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patient_Tracker.Models.Services
+{
+    public class UserServices
+    {
+        private readonly HttpClient client;
+        public UserServices(IHttpClientFactory factory)
+        {
+            client = factory.CreateClient("UserServicesAPI");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+        public async Task<bool> SaveClerk(Clerk register)
+        {
+            var Json = JsonConvert.SerializeObject(register);
+            var Content = new StringContent(Json, Encoding.UTF8, "application/json");
+
+            var Response = await client.PostAsync("/api/auth/clerkregister", Content);
+            Response.EnsureSuccessStatusCode();
+            return Response.StatusCode == HttpStatusCode.Created;
+        }
+        public async Task<bool> SaveDoctor(Doctor register)
+        {
+            var Json = JsonConvert.SerializeObject(register);
+            var Content = new StringContent(Json, Encoding.UTF8, "application/json");
+
+            var Response = await client.PostAsync("/api/auth/doctorregister", Content);
+            Response.EnsureSuccessStatusCode();
+            return Response.StatusCode == HttpStatusCode.Created;
+        }
+        public async Task<bool> SavePatient(Patient register)
+        {
+            var Json = JsonConvert.SerializeObject(register);
+            var Content = new StringContent(Json, Encoding.UTF8, "application/json");
+
+            var Response = await client.PostAsync("/api/auth/patientregister", Content);
+            Response.EnsureSuccessStatusCode();
+            return Response.StatusCode == HttpStatusCode.Created;
+        }
+    }
+}
