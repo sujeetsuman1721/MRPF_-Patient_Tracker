ad75b17307dd4f9f876af54a81bc2284190f1a50
sujeetsuman1721 <sujeetsuman1721@gmail.com>
2022-04-09 13:47:46 +0530

    the final folder is added

---
+++ Billing_Services/BillingServiceTesting/Billing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Billing_Services;
+
+namespace Billing
+{
+    [TestFixture]
+   public class Billing
+    {
+        [Test]
+        public void Patients_WithSame_AppointmentIds_MustBeEqual()
+        {
+            var patient1 = new Bill(1,1,200,300,350,850);
+            var patient2 = new Bill(2,2,100,400,600,1100);
+
+            patient1.Id = 101;
+            patient2.Id = 102;
+
+            var result = patient1.Equals(patient2);
+            Assert.That(result, Is.True);
+        }
+        [Test]
+        public void Create_New_Instance_Using_ValidValues()
+        {
+            var patient1 = new Bill(2,3,450,150,400,1000);
+            Assert.That(patient1, Is.Not.Null);
+        }
+        [Test]
+        [TestCase(-8,2,450,150,400,1000)]
+        [TestCase(-1, 3,100,200,200,500)]
+        [TestCase(1,2, -500,200,400,600)]
+       
+        public void Throw_ArgumentException_For_Invalid_Input(int Id,int AppointmentId,int ConsultationCharges,int LabTestCharges,int RoomCharges,long TotalAmount)
+        {
+            Assert.Throws<ArgumentException>(() => new Bill(Id,AppointmentId,ConsultationCharges,LabTestCharges,RoomCharges,TotalAmount));
+        }
+        [Test]
+        public void Changes_AppointmentId_To_NewId()
+        {
+            var patient = new Bill(1,2,300,300,400,1000);
+            patient.ChangeAppointmentId(5);
+            Assert.That(patient.Id, Is.EqualTo(5));
+        }
+    }
+}
---
+++ Maintain_Patient_Info/PatientInfiTesting/MaintainPatient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Maintain_Patient_Info;
+
+
+namespace MaintainPatient
+{
+    [TestFixture]
+    public class MaintainPatient
+    {
+        [Test]
+        public void Patients_WithSame_AppointmentIds_MustBeEqual()
+        {
+            var patient1 = new MaintainPatientInfo(1,"fever","50",1,2,2,"pending",1,2,2,"blood test","Completed","Double");
+            var patient2 = new MaintainPatientInfo(2, "stomach pain", "100", 3, 4, 1, "pending", 1, 2, 2, "blood test", "Completed", "Double");
+
+            patient1.Id = 101;
+            patient2.Id = 102;
+
+            var result = patient1.Equals(patient2);
+            Assert.That(result, Is.True);
+        }
+        [Test]
+        public void Create_New_Instance_Using_ValidValues()
+        {
+            var patient1 = new MaintainPatientInfo(1,"fever","50",2,3,3,"pending",2,5,1,"blood test","completed","double");
+            Assert.That(patient1, Is.Not.Null);
+        }
+        [Test]
+        [TestCase(-8,"fever","applied",2,2,4,"pending",4,5,1,"blood test","completed","basic")]
+        [TestCase(8, "", "applied", 2, 2, 4, "pending", 4, 5, 1, "blood test", "completed", "basic")]
+        [TestCase(7, "fever", null, 3, 3, 6, "completed", 6, 2, 1, "blood test", "pending", "basic")]
+        public void Throw_ArgumentException_For_Invalid_Input(int ConsultationId,string Purpose,string Charge,int Id,int PatientId,int DoctorId,string Status,int AppointmentId,int RoomId,int LabTestId,string LabTestName,string LabTestResult,string RoomType)
+        {
+            Assert.Throws<ArgumentException>(() => new MaintainPatientInfo(ConsultationId,Purpose,Charge,Id,PatientId,DoctorId,Status,AppointmentId,RoomId,LabTestId,LabTestName,LabTestResult,RoomType));
+        }
+        [Test]
+        public void Change_PatientId_To_NewId()
+        {
+            var patient = new MaintainPatientInfo(1,"fever","applied",2,2,4,"pending",4,5,1,"blood test","completed","basic");
+            patient.ChangePatientId(5);
+            Assert.That(patient.Id, Is.EqualTo(5));
+        }
+        [Test]
+        public void Throw_ArgumentException_When_Invalid_Name_Provided_To_ChangeLabTestName()
+        {
+            var patient = new MaintainPatientInfo(1, "fever", "applied", 2, 2, 4, "pending", 4, 5, 1, "blood test", "completed", "basic");
+            Assert.Throws<ArgumentException>(() => patient.ChangeLabTestName(""));
+            Assert.Throws<ArgumentException>(() => patient.ChangeLabTestName(null));
+        }
+        [Test]
+        public void Throw_ArgumentException_When_Invalid_Name_Provided_To_ChangeLabTestResult()
+        {
+            var patient = new MaintainPatientInfo(1, "fever", "applied", 2, 2, 4, "pending", 4, 5, 1, "blood test", "completed", "basic");
+            Assert.Throws<ArgumentException>(() => patient.ChangeLabTestResult(""));
+            Assert.Throws<ArgumentException>(() => patient.ChangeLabTestResult(null));
+        }
+    }
+}
